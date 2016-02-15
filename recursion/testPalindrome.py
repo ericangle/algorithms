55f73d64b44fdddef6fa07b3ba9481f1b45d6445
Eric Angle <eric.lohner.angle@gmail.com>
2016-02-15 05:38:03 -0700

    all tests passing

--- recursion/testPalindrome.py
+++ recursion/testPalindrome.py
@@ -18,8 +18,14 @@ class test_remove_spaces (unittest.TestCase):
         self.assertEquals (remove_spaces ("a b c "), "abc")
     def test_remove_space_before_and_after(self):
         self.assertEquals (remove_spaces (" a b c "), "abc")
-    def test_raise_typerror(self):
+    def test_special_characters(self):
+        self.assertEquals (remove_spaces (" ! 9 & "), "!9&")
+    def test_raise_typerror_int(self):
         self.assertRaises (TypeError, lambda: remove_spaces (1))
+    def test_raise_typerror_float(self):
+        self.assertRaises (TypeError, lambda: remove_spaces (1.0))
+    def test_raise_typerror_list(self):
+        self.assertRaises (TypeError, lambda: remove_spaces ([1,2,3]))
 
 class test_palindrome (unittest.TestCase):
     def test_none(self):
@@ -38,8 +44,20 @@ class test_palindrome (unittest.TestCase):
         self.assertTrue (palindrome (remove_spaces ("never odd or even")))
     def test_rat(self):
         self.assertTrue (palindrome (remove_spaces ("Was It a Rat I saW")))
+    def test_rat_diff_case(self):
+        self.assertFalse (palindrome (remove_spaces ("was It a Rat I saW")))
     def test_not(self):
         self.assertFalse (palindrome (remove_spaces ("i'm not a palindrome")))
+    def test_special_characters_palindrome(self):
+        self.assertTrue (palindrome (remove_spaces ("! 9 & 9 !")))
+    def test_special_characters_no_palindrome(self):
+        self.assertFalse (palindrome (remove_spaces ("! 9 & 8 !")))
+    def test_raise_typerror_int(self):
+        self.assertRaises (TypeError, lambda: palindrome (1))
+    def test_raise_typerror_float(self):
+        self.assertRaises (TypeError, lambda: palindrome (1.0))
+    def test_raise_typerror_list(self):
+        self.assertRaises (TypeError, lambda: palindrome ([1,2,3]))
 
 if __name__ == '__main__':
     unittest.main()
