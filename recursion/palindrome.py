d53502fdf92424fae5585d3bf7dcfc21f7ef9806
Eric Angle <eric.lohner.angle@gmail.com>
2016-02-15 16:25:28 -0700

    formatting

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -5,21 +5,23 @@ def remove_spaces(myStr):
     return None
   elif not isinstance(myStr, basestring):
     raise TypeError
-  elif len(myStr) == 0:
-    return myStr
-  elif myStr[0] == " ":
-    return remove_spaces(myStr[1:])
   else:
-    return myStr[0] + remove_spaces(myStr[1:])
+    if len(myStr) == 0:
+      return myStr
+    elif myStr[0] == " ":
+      return remove_spaces(myStr[1:])
+    else:
+      return myStr[0] + remove_spaces(myStr[1:])
  
 def palindrome(myStr):
   if myStr == None:
     return False
   elif not isinstance(myStr, basestring):
     raise TypeError
-  elif len(myStr) <= 1:
-    return True
-  elif myStr[0] == myStr[-1]:
-    return palindrome(myStr[1:len(myStr)-1])
   else:
-    return False 
+    if len(myStr) <= 1:
+      return True
+    elif myStr[0] == myStr[-1]:
+      return palindrome(myStr[1:len(myStr)-1])
+    else:
+      return False 
